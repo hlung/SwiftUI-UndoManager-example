d1994273da188bf448ff0e9722d00e6a9af30b17
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2025-03-29 21:00:51 +0800

    Initial Commit

---
+++ LabUndoManager/LabUndoManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabUndoManagerApp.swift
+//  LabUndoManager
+//
+//  Created by Hlung on 29/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LabUndoManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LabUndoManagerTests/LabUndoManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LabUndoManagerTests.swift
+//  LabUndoManagerTests
+//
+//  Created by Hlung on 29/3/25.
+//
+
+import Testing
+@testable import LabUndoManager
+
+struct LabUndoManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
